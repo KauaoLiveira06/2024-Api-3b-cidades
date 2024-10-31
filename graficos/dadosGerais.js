0b6f156021f10ea34f8275828636b1497556e1c4
Kauã dos Reis de Oliveira <127523958+KauaoLiveira06@users.noreply.github.com>
2024-10-31 09:30:19 -0300

    aula 31/10

--- graficos/dadosGerais.js
+++ graficos/dadosGerais.js
@@ -3,13 +3,14 @@ const url = 'https://raw.githubusercontent.com/KauaoLiveira06/2024-Api-3b-cidade
 async function visualizarInfo() {
     const res = await fetch(url)
     const dados = await res.json()
-    
-    const cidades = Object.keys(dados)
-    const cidadesMaisVisitadas = cidades(0)
-    const quantidadeDeVisitantes = Object.values(dados)(0)
+    const totalDeVotos = (dados.cidadeMaisBonita)
 
-    console.log(cidadesMaisVisitadas)
-    console.log(quantidadeDeVisitantes)
+    let paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Nessa pesquisa buscou-se compreender qual as cidades mais bonitas votadas pelos alunos do ensino fundamental e médio. A(o) ${cidadeMaisBonita} foi a cidade mais votada entre todos, com um total de ${quantidadeDeVotos} votos.`
+
+    let container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
 visualizarInfo()
