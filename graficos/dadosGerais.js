3c6a372d34d89dfee08ab242aa23a3924f69fcec
Kauã dos Reis de Oliveira <127523958+KauaoLiveira06@users.noreply.github.com>
2024-11-21 10:47:36 -0300

    aula 21/11

--- graficos/dadosGerais.js
+++ graficos/dadosGerais.js
@@ -10,14 +10,36 @@ async function visualizarInfo() {
     const cidadeMaisVotada = cidadesVotadas[0]
     const quantidadeDeVotos = votos[0]
 
-    console.log(votos)
+   
 
-    let paragrafo = document.createElement('p')
+    const data = [
+        {
+            x: cidadesVotadas,
+            y: votos,
+            type: 'bar',
+        }
+    ]
+
+
+
+
+
+
+   
+
+    const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Nessa pesquisa buscou-se compreender qual as cidades mais bonitas votadas pelos alunos do ensino fundamental e médio. ${cidadeMaisVotada} foi a cidade mais votada entre todos, com um total de ${quantidadeDeVotos} votos.`
+    paragrafo.innerHTML = `Nessa pesquisa buscou-se compreender qual as cidades mais bonitas votadas pelos alunos do ensino fundamental e médio. <span>${cidadeMaisVotada}</span> foi a cidade mais votada entre todos, com um total de <span>${quantidadeDeVotos}</span> votos.`
 
-    let container = document.getElementById('graficos-container')
+    const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
+
+    const graficos = document.createElement('div')
+    graficos.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(graficos)
+    Plotly.nemPlot(graficos,data)
 }
+
+
 
 visualizarInfo()
